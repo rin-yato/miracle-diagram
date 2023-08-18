5eb49b5dada1a76f730626825ecec21a5a52ef42
rin-yato <chearithorn@gmail.com>
2023-08-18 13:56:46 +0700

    feat: added support for contraints and relationship

--- src/lib/lang-miro/grammar/miro.ts
+++ src/lib/lang-miro/grammar/miro.ts
@@ -3,15 +3,15 @@ import {LRParser} from "@lezer/lr"
 import {MiroStyleTags} from "../highlight"
 export const parser = LRParser.deserialize({
   version: 14,
-  states: "$[OVQPOOO[QPO'#C^QOQPOOOaQQO,58xOiQSO'#C`OnQPO'#ClOOQO1G.d1G.dOvQPO1G.dO{QWO,58zO!ZQQO'#CfO!fQPO,59WOOQO7+$O7+$OO!nQWO'#CmO!|QPO'#CdOOQO1G.f1G.fO#RQPO1G.fOOQO,59Q,59QOOQO-E6d-E6dOOQO'#Ce'#CeO#^QWO,59XO#lQPO,59OOOQO7+$Q7+$QOOQO-E6c-E6cO#qQQO1G.jOOQO7+$U7+$U",
-  stateData: "#v~O]OS~ORPO~O_RO~OTSO^UO~OUWO~OdXO^`X~O^ZO~OV[Ob]O^SadSa~OTSO^YXdYX~OdXO^`a~OVbO^aXbaXdaX~ORdO~Ob]O^SidSi~OVbO^aabaadaa~OcgO~OThO~O",
-  goto: "!UbPPcPfPPPlrxPPPPP!O!RRQOQTRR`XQ^WRe_Qc[RfcQYTRaYRVRR_W",
-  nodeNames: "⚠ Database Table TableName Column ColumnName ColumnType Constraint Relationship",
+  states: "$[OVQPOOO[QPO'#C_QOQPOOOaQQO,58yOiQSO'#CbOnQPO'#ClOOQO1G.e1G.eOvQPO1G.eO{QWO,58|O!ZQQO'#ChO!fQPO,59WOOQO7+$P7+$PO!nQWO'#CmO!|QPO'#CfOOQO1G.h1G.hO#RQPO1G.hOOQO,59S,59SOOQO-E6f-E6fOOQO'#Cg'#CgO#^QWO,59XO#lQPO,59QOOQO7+$S7+$SOOQO-E6e-E6eO#qQQO1G.lOOQO7+$W7+$W",
+  stateData: "#v~O_OS~OSPO~OTRO~OQUOVSO~OWWO~OdXOQ`X~OQZO~OX[Ob]OQUadUa~OVSOQ[Xd[X~OdXOQ`a~OXbOQaXbaXdaX~OSdO~Ob]OQUidUi~OXbOQaabaadaa~OcgO~OVhO~O",
+  goto: "!UbPPPcPPfPPPlrxPPP!O!RRQOQTRR`XQ^WRe_Qc[RfcQYTRaYRVRR_W",
+  nodeNames: "⚠ Database } Table TableName { Column ColumnName ColumnType Constraint Relationship",
   maxTerm: 20,
   propSources: [MiroStyleTags],
   skippedNodes: [0],
   repeatNodeCount: 2,
-  tokenData: "#R~R[XYwYZw]^wpqw|}|}!O!R!O!P!^!c!}!c#R#S!c#T#o!c#o#p!w#q#r!|~|O]~~!ROd~~!UP!`!a!X~!^Ob~~!cOc~_!nRTQUSVWRP!c!}!c#R#S!c#T#o!c~!|O_~~#RO^~",
+  tokenData: "#R~R[XYwYZw]^wpqw|}|}!O!R!O!P!^!c!}!c#R#S!c#T#o!c#o#p!w#q#r!|~|O_~~!ROd~~!UP!`!a!X~!^Ob~~!cOc~_!nRVQWSXWSP!c!}!c#R#S!c#T#o!c~!|OT~~#ROQ~",
   tokenizers: [0, 1, 2, 3],
   topRules: {"Database":[0,1]},
   tokenPrec: 0
---
+++ src/lib/lang-miro/index.ts
@@ -0,0 +1,109 @@
+import {
+  LRLanguage,
+  LanguageSupport,
+  continuedIndent,
+  foldInside,
+  foldNodeProp,
+  indentNodeProp,
+  syntaxTree,
+} from '@codemirror/language';
+import { parser } from './grammar/miro';
+import {
+  autocompletion,
+  completeFromList,
+  completeAnyWord,
+  CompletionContext,
+  CompletionResult,
+} from '@codemirror/autocomplete';
+import { MiroHighlighting, MiroStyleTags } from './highlight';
+import { linter, Diagnostic } from '@codemirror/lint';
+
+let parserWithMetadata = parser.configure({
+  props: [
+    MiroStyleTags,
+    indentNodeProp.add({
+      Table: continuedIndent(),
+    }),
+    foldNodeProp.add({
+      Table: cx => {
+        const fold = foldInside(cx.node);
+        if (!fold) return null;
+        return {
+          from: fold.from + 2,
+          to: fold.to,
+        };
+      },
+    }),
+  ],
+});
+
+export const miroLanguage = LRLanguage.define({
+  name: 'miro',
+  parser: parserWithMetadata,
+});
+
+export const miroDefaultCompletion = completeFromList([
+  { label: 'string', type: 'keyword' },
+  { label: 'number', type: 'keyword' },
+  { label: 'boolean', type: 'keyword' },
+  { label: 'int', type: 'keyword' },
+  { label: 'float', type: 'keyword' },
+  { label: 'date', type: 'keyword' },
+]);
+
+const miroTypeCompletion = (
+  context: CompletionContext,
+): CompletionResult | null => {
+  // match if there is a word and space
+  const match = context.matchBefore(/\w\s+\w+/);
+
+  if (match) {
+    return {
+      from: context.pos,
+      options: [
+        { label: 'string', type: 'type' },
+        { label: 'number', type: 'type' },
+        { label: 'boolean', type: 'type' },
+        { label: 'int', type: 'type' },
+        { label: 'float', type: 'type' },
+        { label: 'date', type: 'type' },
+      ],
+    };
+  }
+
+  return null;
+};
+
+const MiroLinter = linter(view => {
+  let diagnostics: Diagnostic[] = [];
+  syntaxTree(view.state)
+    .cursor()
+    .iterate(node => {
+      if (node.type.isError)
+        diagnostics.push({
+          from: node.from,
+          to: node.to,
+          severity: 'error',
+          message: 'Syntax Error!',
+          actions: [
+            {
+              name: 'Remove',
+              apply(view, from, to) {
+                view.dispatch({ changes: { from, to } });
+              },
+            },
+          ],
+        });
+    });
+  return diagnostics;
+});
+
+export function miro() {
+  return new LanguageSupport(miroLanguage, [
+    MiroHighlighting,
+    MiroLinter,
+    autocompletion({
+      override: [miroTypeCompletion, completeAnyWord],
+    }),
+  ]);
+}
