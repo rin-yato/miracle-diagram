f3bb7905c78d67a183374995e42d2ace62396a7a
rin-yato <chearithorn@gmail.com>
2023-08-23 00:41:25 +0700

    feat: added split view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import { ThemeToggle } from '@/components/theme/toggle';
+'use client';
+
+import { CodeEditor } from '@/components/code-editor';
+import { Flow } from '@/components/flow';
+import { Sash } from '@/components/sash';
+import { useState } from 'react';
+import SplitPane from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css';
 
 export default function Home() {
+  const [sizes, setSizes] = useState<(number | string)[]>(['25%', 'auto']);
   return (
-    <main>
-      <ThemeToggle />
+    <main className="w-full h-full flex overflow-hidden">
+      <SplitPane
+        split="vertical"
+        sizes={sizes}
+        onChange={setSizes}
+        sashRender={Sash}
+      >
+        <aside className="bg-secondary h-full overflow-y-auto">
+          <CodeEditor />
+        </aside>
+        <section className="flex-1 w-full h-full">
+          <Flow />
+        </section>
+      </SplitPane>
     </main>
   );
 }
---
+++ src/components/sash.tsx
@@ -0,0 +1,8 @@
+export function Sash(index: number, active: boolean) {
+  return (
+    <div
+      key={index + '-sash'}
+      className="w-0.5 relative hover:ring-8 before:absolute before:inset-y-0 before:-inset-x-3 before:bg-transparent ring-neutral-200/40 dark:ring-neutral-700/80 duration-300 h-full bg-border"
+    ></div>
+  );
+}
