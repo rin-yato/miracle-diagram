02ecdf30c3f657ebf7267a48ef53ad2d05f16775
rin-yato <chearithorn@gmail.com>
2023-08-24 00:29:58 +0700

    feat: added million integration

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import million from 'million/compiler';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const millionConfig = {
+  auto: { rsc: true },
+};
+
+export default million.next(nextConfig, millionConfig);
